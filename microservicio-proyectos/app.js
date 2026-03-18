351eab864781e0a666762c484d83216e69399522
shalo <yepessalome205@outlook.com>
2026-03-18 00:00:28 -0500

    Implementación de escalamiento horizontal y balanceo de carga con Nginx

--- microservicio-proyectos/app.js
+++ microservicio-proyectos/app.js
@@ -6,6 +6,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+// Middleware para ver qué instancia responde
+app.use((req, res, next) => {
+    console.log(`Petición atendida por la instancia: ${process.env.HOSTNAME || 'Desconocida'}`);
+    next();
+});
+
 // Routes
 app.use('/api/proyectos', require('./routes/proyecto'));
 
